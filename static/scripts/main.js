e5666b702abb0e2248c5ce795419adb68dbe44d0
Yamin Mahdi <yamin_khan@asia.com>
2024-06-29 12:38:16 +0600

    ui fix

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -5,12 +5,23 @@ const asyncGetPrediction = async () => {
             const data = await response.json();
             console.log(data);
             $(".loading").hide();
-            $(".predTxt").text(data["class"]);
+            const cls = data["class"];
+            if(cls === "Normal")
+                $('.predTxt').css('color', '#4beb4b');
+            else
+                $('.predTxt').css('color', '#ff7373');
+            $(".predTxt").text(cls);
             $(".predPer").text("Accuracy: " + data["accuracy"]);
             $(".pred").show();
             $(".getPrediction").show();
 
         } catch(error) {
+            $(".loading").hide();
+            $('.predTxt').css('color', '#ff7373');
+            $(".predTxt").text(error);
+            $(".predPer").text("");
+            $(".pred").show();
+            $(".getPrediction").show();
             console.log(error)
         } 
     }
